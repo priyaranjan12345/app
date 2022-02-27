546ad3ea8a551b8c76055176b7766d4eb9f467b8
Priyaranjan <priyaranjanmantri@gmail.com>
2022-02-27 22:24:37 +0530

    10th

--- blog/routers/profile.py
+++ blog/routers/profile.py
@@ -1,6 +1,7 @@
 from fastapi import status, APIRouter, UploadFile, File, Form, Depends, HTTPException
+from fastapi.responses import FileResponse
 from typing import List
-import shutil
+import shutil, glob
 
 approute = APIRouter(tags=["Profile"])
 
@@ -13,7 +14,6 @@ async def uploadProfileData(file: UploadFile = File(...)):
         
     return {'status': status.HTTP_201_CREATED, 'message': 'Profile data uploaded successfully'}
 
-
 @approute.post('/uploadMultipleFiles', status_code=status.HTTP_201_CREATED,)
 async def uploadFiles(files: List[UploadFile] = File(...)):
     for file in files:
@@ -22,6 +22,21 @@ async def uploadFiles(files: List[UploadFile] = File(...)):
             shutil.copyfileobj(file.file, buffer)
         
     return {'status': status.HTTP_201_CREATED, 'message': 'Profile data uploaded successfully'}
+
+@approute.get('/downloadFile')
+def getAllFiles():
+    return FileResponse(path='files/one.png', filename='one.png', media_type='application/octet-stream')
+
+@approute.get('/allFineNames')
+def allFilesName():
+    txtfiles = []
+    for file in glob.glob("files/*.*"):
+        txtfiles.append(file)
+    return {'files': txtfiles}
+
+@approute.post('/userData')
+def userData(name: str = Form(...), dob: str = Form(...),file: UploadFile = File(...)):
+    return {'name': name, 'file': file.filename, 'dob': dob}
 
 
 
