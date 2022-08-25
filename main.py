c9e054d157aaab63ff01aaba5e63801bed3460e7
Priyaranjan <Priyaranjan.M@lucidplus.com>
2022-08-25 11:00:28 +0530

    cors added

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from blog.routers import userRouter, blogRouter, authRouter, profile
 from blog import models
 from blog import database
 import uvicorn
 
 app = FastAPI()
+
+origins = [
+    "https://app-demo-banty.herokuapp.com",
+    "http://app-demo-banty.herokuapp.com",
+    "http://localhost:8000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get('/')
 def homeView():
